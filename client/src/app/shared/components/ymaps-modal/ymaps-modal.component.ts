814a7373718cfe515a49012e4e262ad5c2bbcf1f
katol62 <katol62@gmail.com>
2020-11-09 15:22:26 +0300

    client - yandex maps - loading

--- client/src/app/shared/components/ymaps-modal/ymaps-modal.component.ts
+++ client/src/app/shared/components/ymaps-modal/ymaps-modal.component.ts
@@ -1,6 +1,9 @@
 import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ModalController} from "@ionic/angular";
 import ymaps from 'ymaps';
+import {LoaderService} from "../../services/loader.service";
+
+export const yMapsUrl = '//api-maps.yandex.ru/2.1/?lang=ru_RU';
 
 @Component({
     selector: 'app-ymaps-modal',
@@ -15,29 +18,29 @@ export class YmapsModalComponent implements OnInit {
     @ViewChild("maps", { read: ElementRef })
     private searchbarElem: ElementRef;
 
-    constructor(private modalController: ModalController) { }
+    constructor(
+        private modalController: ModalController,
+        private loaderService: LoaderService) { }
 
     ngOnInit() {
         this.loadMaps();
     }
 
-    loadMaps(): void {
-
-        if (this.loaded) {
-            return;
+    async loadMaps(): Promise<void> {
+        await this.loaderService.loadingPresent();
+        try {
+            const maps = await ymaps.load(yMapsUrl);
+            this.map = new maps.Map('maps-container', {
+                center: [44.946329, 34.104218],
+                zoom: 19
+            });
+            await this.loaderService.loadingDismiss();
+        } catch (e) {
+            await this.loaderService.loadingDismiss();
         }
-        ymaps.load('//api-maps.yandex.ru/2.1/?lang=ru_RU').then(
-            maps => {
-                const map = new maps.Map('maps-container', {
-                    center: [44.946329, 34.104218],
-                    zoom: 18
-                });
-                this.loaded = true;
-            }
-        )
-            .catch( error => {
-                console.log(error);
-            })
+        finally {
+            await this.loaderService.loadingDismiss();
+        }
     }
 
     closeModal() {
